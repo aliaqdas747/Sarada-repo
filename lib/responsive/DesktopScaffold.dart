225d5991fa953d2928e9c818e1e28c62079fd04b
aliaqdas747 <142832510+aliaqdas747@users.noreply.github.com>
2023-11-20 22:17:25 +0500

    Animation complete

--- lib/responsive/DesktopScaffold.dart
+++ lib/responsive/DesktopScaffold.dart
@@ -12,6 +12,19 @@ class desktopScaffold extends StatefulWidget{
 }
 
 class _mobileScaffoldState extends State<desktopScaffold> {
+  bool isExpanded = false;
+  double containerHeight = 50;
+  double containerWidth = 40;
+  double opacityValue = 0.0;
+
+
+
+   Decoration my_dec =BoxDecoration(
+     color: Color(0xFF10EFB1),
+     borderRadius: BorderRadius.circular(10),
+
+   );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,6 +47,58 @@ class _mobileScaffoldState extends State<desktopScaffold> {
                           mainAxisAlignment: MainAxisAlignment.center,
                           crossAxisAlignment: CrossAxisAlignment.center,
                           children: [
+
+
+
+
+                            //Animated Container
+                            GestureDetector(
+                              onTap: (){
+                                setState(() {
+                                  if(isExpanded){
+                                    containerHeight=40;
+                                    containerWidth=40;
+                                    opacityValue=0;
+
+
+                                  }else{
+                                    containerHeight=300;
+                                    opacityValue=1;
+                                  }
+                                  isExpanded=!isExpanded;
+                                });
+                              },
+                              child: AnimatedContainer(
+                                duration: Duration(seconds: 2),
+                                width: containerWidth,
+                                height: containerHeight,
+                                decoration: my_dec,
+                                child: AnimatedOpacity(
+                                  opacity: opacityValue,
+                                  duration: Duration(seconds: 2,),
+                                  child: Center(
+                                    child: Column(
+                                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                      children: [
+                                        Icon(Icons.account_circle),
+                                        Icon(Icons.language_sharp),
+                                        Icon(Icons.facebook),
+                                        Icon(Icons.mail),
+
+                                      ],
+                                    ),
+                                  ),
+
+
+                                ),
+                              ),
+
+                            ),
+
+
+
+
+
                             SizedBox(width: 100,),
                             SizedBox(
                               child: Column(
@@ -50,7 +115,8 @@ class _mobileScaffoldState extends State<desktopScaffold> {
                                     child:animated_text,
                                   ),
                                   SizedBox(height: 50,),
-                                  lets_chat
+                                  lets_chat,
+
                                 ],
                               ),
                             ),
